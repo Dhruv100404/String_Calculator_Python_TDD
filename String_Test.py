8ff1250230923b0eef7bd3dcbc431e79bfb24aff
Dhruv Thakkar <106736371+Dhruv100404@users.noreply.github.com>
2024-08-01 16:54:32 +0530

    Test for Empty String

--- String_Test.py
+++ String_Test.py
@@ -1,8 +1,10 @@
 import unittest
-
+from String_Calc import Calculator
 
 class TestCalculator(unittest.TestCase):
-    pass
+
+    def test_should_return_zero_on_empty_string(self):
+        self.assertEqual(Calculator.add(""), 0)
 
 if __name__ == '__main__':
     unittest.main()
