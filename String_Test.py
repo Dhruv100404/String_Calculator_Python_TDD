d8feeecae69619135ce05ac0f846e785492b897a
Dhruv Thakkar <106736371+Dhruv100404@users.noreply.github.com>
2024-08-01 16:56:26 +0530

    Test for Single Number

--- String_Test.py
+++ String_Test.py
@@ -6,5 +6,8 @@ class TestCalculator(unittest.TestCase):
     def test_should_return_zero_on_empty_string(self):
         self.assertEqual(Calculator.add(""), 0)
 
+    def test_should_return_number_on_single_string(self):
+        self.assertEqual(Calculator.add("1"), 1)
+
 if __name__ == '__main__':
     unittest.main()
