982a7e95d9858385ff042c00acdb645064e9fa47
Dhruv Thakkar <106736371+Dhruv100404@users.noreply.github.com>
2024-08-01 16:35:47 +0530

    Add Support for Custom Delimiters

--- String_Calc.py
+++ String_Calc.py
@@ -12,6 +12,25 @@ class Calculator:
 
     @staticmethod
     def splitter(numbers):
+        if not numbers:
+            return []
+        elif Calculator.is_custom_delimiter_string(numbers):
+            return Calculator.split_using_custom_delimiter(numbers)
+        return Calculator.split_using_comma_and_new_line(numbers)
+
+    @staticmethod
+    def is_custom_delimiter_string(numbers):
+        return numbers.startswith("//")
+
+    @staticmethod
+    def split_using_custom_delimiter(numbers):
+        m = re.match("//(.)\n(.*)", numbers)
+        custom_delim = m.group(1)
+        num = m.group(2)
+        return re.split(re.escape(custom_delim), num)
+
+    @staticmethod
+    def split_using_comma_and_new_line(numbers):
         return re.split(",|\n", numbers)
 
     @staticmethod
